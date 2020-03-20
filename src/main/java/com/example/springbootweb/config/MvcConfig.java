fb11d9dcd088c1a45230840be53e3ace3c9f7cee
yujin12315 <yujin19861013@163.com>
2020-03-21 07:23:39 +0800

    modiy

--- src/main/java/com/example/springbootweb/config/MvcConfig.java
+++ src/main/java/com/example/springbootweb/config/MvcConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-//    //     我们自己编写一个视图解析，路由！
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        // 视图跳转的控制！
-//        registry.addViewController("/index").setViewName("login");
-//        registry.addViewController("/").setViewName("login");
-//        registry.addViewController("/index.html").setViewName("login");
-//    }
+    //     我们自己编写一个视图解析，路由！
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 视图跳转的控制！
+        registry.addViewController("/index").setViewName("login");
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index.html").setViewName("login");
+    }
 
 
 
